5ae176a69bf1d5f6fe182f65d38b5497a50ae8a1
LeandroCampos123 <leandro.silva388@fatec.sp.gov.br>
2025-10-15 23:38:24 -0300

    feat (Scrum-35): Implementar funcionalidade cadastrar Seção API ao banco de dados

--- TechnoCode/src/main/java/com/example/technocode/Controllers/FormularioApiController.java
+++ TechnoCode/src/main/java/com/example/technocode/Controllers/FormularioApiController.java
@@ -1,15 +1,31 @@
 package com.example.technocode.Controllers;
 
+import com.example.technocode.dao.Connector;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FormularioApiController {
     @FXML
     private ChoiceBox<String> choiceBoxSemestre,choiceBoxSemestreDoCurso;
+    @FXML
+    private TextField txtAno, txtEmpresa, txtLinkRepositorio;
+    @FXML
+    private TextArea txtProblema, txtSolucao, txtTecnologias, txtContribuicoes, txtHardSkills, txtSoftSkills;
 
     @FXML
     private void initialize(){
         choiceBoxSemestre.getItems().addAll("1","2");
         choiceBoxSemestreDoCurso.getItems().addAll("Primeiro semestre","Segundo semestre","Terceiro semestre","Quarto semestre","Quinto semestre","Sexto semestre");
+    }
+    @FXML
+    private void enviarSecaoApi(ActionEvent event) {
+        Connector connector = new Connector();
+        connector.cadastrarSessaoApi("@email", choiceBoxSemestreDoCurso.getValue(), Integer.parseInt(txtAno.getText()), choiceBoxSemestre.getValue(), 1,
+                txtEmpresa.getText(), txtProblema.getText(), txtSolucao.getText(),txtLinkRepositorio.getText(),txtTecnologias.getText(),
+                txtContribuicoes.getText(),txtHardSkills.getText(),txtSoftSkills.getText());
+
     }
 }
